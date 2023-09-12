fe3a5fd3f612597ada1f098386f3aeb9a4c4125c
Alineopsoliveira <alinep730@gmail.com>
2023-09-12 01:37:47 -0300

    api conectando ao banco de dados

---
+++ middlewares/conectaMongoDB.ts
@@ -0,0 +1,8 @@
+import type {NextApiRequest , NextApiResponse, NextApiHandler } from 'next';
+import mongoose from 'mongoose'
+
+
+export const conectarMongoDB = (handler : NextApiHandler) => 
+(req: NextApiRequest, res: NextApiResponse) => {
+
+}

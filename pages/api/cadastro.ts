ed69e0a1cf6eae751b7ae54d4d762e22e60641f4
Alineopsoliveira <alinep730@gmail.com>
2024-03-14 21:44:51 -0300

    atualizações do devagram

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -3,46 +3,66 @@ import type { RespostaPadraoMsg } from "@/types/RepostaPadraoMsg";
 import type { CadastroRequisicao } from "@/types/CadastroRequisicao";
 import   { UsuarioModel } from  "../../models/UsuarioModel";
 import { conectarMongoDB } from "@/middlewares/conectarMongoDB";
-
-
 import md5 from 'md5';
-
-const endpointCadastro = 
-    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
-
-    if(req.method === 'POST'){
-        const usuario = req.body as CadastroRequisicao;
+import { upload, uploadImagemCosmic } from "@/services/uploadImagemCosmic";
+import nc from 'next-connect';
 
 
-        if(!usuario.nome || usuario.nome.length < 2){
-            return res.status(400).json({erro : "Nome inválido"});
-        } 
-        if(!usuario.email || usuario.email.length < 5 
-           || !usuario.email.includes ('@')
-           || !usuario.email.includes('.')){
-             return res.status(400).json({erro:"Email inválido"});
-           }
-        if(!usuario.senha || usuario.senha.length < 4){
-            return res.status(400).json({erro:"Senha inválida"});
+const handler = nc ()
+    .use(upload.single('file'))
+    .post( async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+            
+                try{
+                    console.log('cadastro endpoint', req.body); //teste
+                const usuario = req.body as CadastroRequisicao;
+        
+        
+                if(!usuario.nome || usuario.nome.length < 2){
+                    return res.status(400).json({erro : "Nome inválido"});
+                } 
+                if(!usuario.email || usuario.email.length < 5 
+                || !usuario.email.includes ('@')
+                || !usuario.email.includes('.')){
+                    return res.status(400).json({erro:"Email inválido"});
+                }
+                if(!usuario.senha || usuario.senha.length < 4){
+                    return res.status(400).json({erro:"Senha inválida"});
+                }
+        
+                // validação  se ja existe usuario com o mesmo email
+                const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+                if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+                    return res.status(400).json({erro: 'Ja existe uma conta com o email informado'})
+                }
+                
+                //enviar a imagem do multer para o cosmic
+                const image = await uploadImagemCosmic(req)
+                console.log(image)
+
+                //Salvar no banco de dados
+                const usuarioAserSalvo = {
+                    nome : usuario.nome,
+                    email : usuario.email,
+                    senha : md5(usuario.senha), 
+                    avatar : image?.media?.url
+                }
+                await UsuarioModel.create(usuarioAserSalvo);
+                return res.status(200).json({msg: 'Usuário criado com sucesso'});
+                } 
+                catch(e : any){
+                    console.log(e);
+                    return res.status(400).json({erro : e.toString()});
+                }
+                
+            }
+        
+    
+    )
+
+    export const config = {
+        api: {
+            bodyParser : false
         }
-
-        // validação  se ja existe usuario com o mesmo email
-        const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
-        if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
-            return res.status(400).json({erro: 'Ja existe uma conta com o email informado'})
-        }
-
-
-        const usuarioAserSalvo = {
-            nome : usuario.nome,
-            email : usuario.email,
-            senha : md5(usuario.senha), 
-        }
-        await UsuarioModel.create(usuarioAserSalvo);
-        return res.status(200).json({msg: 'Usuário criado com sucesso'});
-           
     }
-    return res.status(405).json({erro : 'Metodo informado não é válido'});
-}
-
-export default conectarMongoDB(endpointCadastro);
+  
+export default conectarMongoDB(handler);
--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,10 +1,10 @@
-//import type { NextApiRequest, NextApiResponse } from "next";
-//import { ValidarTokenJWT } from '../../middlewares/validarTokenJWT'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ValidarTokenJWT } from '../../middlewares/validarTokenJWT'
 
 
-//const usuarioEndpoint = (req : NextApiRequest, res : NextApiResponse) => {
+const usuarioEndpoint = (req : NextApiRequest, res : NextApiResponse) => {
 
- //   return res.status(200).json('Usuario autenticado com sucess');
-//}
+    return res.status(200).json('Usuario autenticado com sucess');
+}
 
-//export default  ValidarTokenJWT(usuarioEndpoint);
+export default  ValidarTokenJWT(usuarioEndpoint);

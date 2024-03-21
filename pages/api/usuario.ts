bf868693e694dbde97d863548bb016cf253924db
Alineopsoliveira <alinep730@gmail.com>
2024-03-20 21:27:48 -0300

    get do usuario logado e feed pelo id do usuario

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,10 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { validarTokenJWT } from '../../middlewares/validarTokenJWT'
+import type { RespostaPadraoMsg } from "@/types/RepostaPadraoMsg";
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { conectarMongoDB } from "@/middlewares/conectarMongoDB"; 
+
+import { UsuarioModel } from "@/models/UsuarioModel";
 
 
-const usuarioEndpoint = (req : NextApiRequest, res : NextApiResponse) => {
+const usuarioEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any >) => {
 
-    return res.status(200).json('Usuario autenticado com sucess');
+    try{
+
+        const {userId} = req?.query;
+        const usuario = await UsuarioModel.findById(userId);
+        usuario.senha = null
+        return res.status(200).json(usuario)
+
+    } catch (e){
+        console.log(e);
+        return res.status(400).json({erro : "Não foi possivel obter"})
+    }
+
+    
+   
+
+   
 }
 
-export default  validarTokenJWT(usuarioEndpoint);
+export default  validarTokenJWT(conectarMongoDB(usuarioEndpoint));
